a520f0b62ab8ee63f71c493f8d2e81f5b4af3d4b
Túlio Lima <tulio1502@hotmail.com>
2017-09-13 11:14:15 -0300

    sugestão de lógica

--- montador/montador/montador.cpp
+++ montador/montador/montador.cpp
@@ -27,18 +27,27 @@ void lerarquivo() {
 }
 
 void filtro_comentarios() {
-
+	//retira os comentários
+	//assim que encontra um símbolo de comentário pula para a próxiam linha
+	//reescreve esse arquivo em um intermediário
 }
 
 void montagem() {
+	//faz a conversão do código conforme a passagem única
+	//preenche as tabelas de simbolos e uso
+	//checa erros (fazer uma função de detecção de erros da linguagem inventada)
+	//escrever em um arquivo de saida
 
 }
 
 void codigo_objeto() {
-
+	//mostra o código objetos escrito
 }
 
 int main() {
+
+	//verificar como é a chamada do programa pelo gcc
+
 	lerarquivo();
 	filtro_comentarios(); // Podemos salvar em um vetor
 	montagem();
