3aa5a15bd45d7887b2855f435d3c0ff56a78259a
barbads <matheusbmiranda@gmail.com>
2017-10-17 10:04:36 -0300

    mdt

--- montador/montador/montador.cpp
+++ montador/montador/montador.cpp
@@ -182,7 +182,7 @@ void pre_procesamento(char* file_name) {
 
 				//No computador do Túlio a próxima linha é -2, no do Barbosa é -1
 
-				valorparam=line[fim-1];
+				valorparam=line[fim-2];
 
 				posequ=line.find(":");
 				nomeparam=line.substr(0,posequ);
